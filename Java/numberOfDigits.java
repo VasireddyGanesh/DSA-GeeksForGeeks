d2c1c3c50af9b60022c16a4d42c4f706f8340ac4
ganesh <vasireddi.ganesh.8@gmail.com>
2021-05-25 13:00:46 +0530

    completion of First Program

--- Java/numberOfDigits.java
+++ Java/numberOfDigits.java
@@ -1,27 +1,46 @@
 import java.util.Scanner;
+import java.lang.Math.*;
 
-class Number{
-    int num;
-    int findNumberOfDigits(){
-        int count=0;
-        if(num!=0){
-            while(num>0){
-                num/=10;
-                count++;
-            }
-            return count;
-        }else{
-            return 1;
-        }
-        
-    }
-}
+// import java.util.Scanner;
 
-class FindDigitsCount{
+// class Number{
+//     int num;
+//     int findNumberOfDigits(){
+//         int count=0;
+//         if(num!=0){
+//             while(num>0){
+//                 num/=10;
+//                 count++;
+//             }
+//             return count;
+//         }else{
+//             return 1;
+//         }
+
+//     }
+// }
+
+// class FindDigitsCount{
+//     public static void main(String[] args) {
+//         Number n=new Number();
+//         Scanner sc=new Scanner(System.in);
+//         n.num=sc.nextInt();
+//         System.out.println("Number of Digits is "+ n.findNumberOfDigits());
+//     }
+// }
+
+class findNumberOfDigits {
     public static void main(String[] args) {
-        Number n=new Number();
-        Scanner sc=new Scanner(System.in);
-        n.num=sc.nextInt();
-        System.out.println("Number of Digits is "+ n.findNumberOfDigits());
+        int num;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        double count = 0;
+        if (num != 0) {
+            count =Math.floor(java.lang.Math.log10(num))+1;
+        } else {
+            count = 1;
+        }
+        System.out.println("Number of Digits is " + count);
+        sc.close();
     }
 }
